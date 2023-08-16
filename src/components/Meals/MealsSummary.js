677dc3afafebc2a3e745b78bf97018233820aa21
L7Git <lukakhutsiashvili@gmail.com>
2023-08-16 14:37:49 +0400

    deploy food-order-app

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Savor the Taste of Delight, Delivered to You</h2>
+      <p>
+        Indulge in the pleasure of choosing your preferred meal from our
+        extensive menu. Enjoy a delectable lunch or dinner conveniently at home.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
